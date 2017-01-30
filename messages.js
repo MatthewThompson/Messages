d76d28b740bb1ebea7a6db7734146f612956264b
MatthewThompson <mat@matthewjthompson.co.uk>
2017-01-30 04:06:43 +0000

    Changed to create the settings file on app start, if it does not exist.

--- messages.js
+++ messages.js
@@ -229,6 +229,13 @@ function createSettings() {
 // Start the app by listening on port 3000.
 app.listen(3000, function () {
 	console.log('Listening on port 3000...');
+	
+	fs.open("settings.json", "r", (err, fd) => {
+		if (err) {
+			createSettings();
+		}
+	});
+	
 })
 
 
